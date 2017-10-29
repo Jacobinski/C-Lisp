6b732f1051ec3efabade0e98fc7fbd4ef9e6e556
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-10-29 17:24:58 -0600

    Hook in parser to REPL
    The REPL will now parse the input. It returns an abstract syntax
    tree on success, an error message on failure.

--- parsing.c
+++ parsing.c
@@ -33,11 +33,10 @@ puts("Press Ctrl+C to Exit\n");
 while(1)
     {
     /*
-     * This REPL loop takes a user input and echoes it.
-     *
-     * TODO: This may need to sleep to reduce CPU load.
+     * This REPL loop takes a user input and parses it.
      */
     char* input;
+    mpc_result_t r;
 
     /* Read the user input */
     input = readline("C-Lisp> ");
@@ -45,8 +44,19 @@ while(1)
     /* Allow the user to press up to retrieve command */
     add_history(input);
 
-    /* Echo */
-    printf("Echo: %s\n", input);
+    /* Parse the user input */
+    if( mpc_parse("<stdin>", input, program, &r) )
+        {
+        /* Success: Print the abstract syntax tree */
+        mpc_ast_print(r.output);
+        mpc_ast_delete(r.output);
+        }
+    else
+        {
+        /* Failure: Print the error */
+        mpc_err_print(r.error);
+        mpc_err_delete(r.error);
+        }
 
     /* Free the pointer allocated by readline */
     free(input);
